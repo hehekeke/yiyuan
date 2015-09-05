e40af09d3f23c073b2bb8a7e7c93b6c0220d92d6
wyq <751239183@qq.com>
2015-09-05 17:13:34 +0800

    wyq提交

---
+++ Application/Home/Controller/WebsiteController.class.php
@@ -0,0 +1,21 @@
+<?php
+// +----------------------------------------------------------------------
+// | OneThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
+// +----------------------------------------------------------------------
+namespace Home\Controller;
+
+/**
+ * 公众号管理
+ */
+class WebsiteController extends HomeController {
+	public function index()
+	{	
+		 // $this->success('新增成功', 'website/index');
+		// echo 111;
+		$this->display ("");
+	}
+}
